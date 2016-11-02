1df6e693c76464a10888b3e1e557b6f1912b7222
wangxuan <wangxuan@qgpic.com>
2016-11-02 18:25:41 +0800

    add Facades

---
+++ src/Fuguevit/Sms/Facades/Sms.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fuguevit\Sms\Facades;
+
+class Sms extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'sms';
+    }
+
+}
